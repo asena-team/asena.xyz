9e1fc4a561a1ba2093c8739d8e113ba4e388225b
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2021-05-01 18:20:21 +0300

    refactor: preloader comeback
    Signed-off-by: Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>

---
+++ js/preloader.js
@@ -0,0 +1,11 @@
+const preloader = document.querySelectorAll('.preloader_container')[0]
+
+window.onload = function(){
+    setTimeout(function(){
+        preloader.style.opacity = 0
+        document.documentElement.classList.remove('no_scroll')
+        setTimeout(function(){
+            document.body.removeChild(preloader)
+        }, 200)
+    }, 250)
+}
