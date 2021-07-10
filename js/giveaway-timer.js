7fb5551fa627b34cccd35ea23455e96134342bd7
fatihege <fatihxege@gmail.com>
2021-07-10 14:38:35 +0300

    Set second to 60

--- js/giveaway-timer.js
+++ js/giveaway-timer.js
@@ -2,7 +2,6 @@ const remainingElem = {
     main: document.querySelector('.timer'),
     end: document.querySelector('.timer-end'),
     month: document.querySelector('.timer .timer-unit.month .number'),
-    week: document.querySelector('.timer .timer-unit.week .number'),
     day: document.querySelector('.timer .timer-unit.day .number'),
     hour: document.querySelector('.timer .timer-unit.hour .number'),
     minute: document.querySelector('.timer .timer-unit.minute .number'),
@@ -10,10 +9,15 @@ const remainingElem = {
 }
 const endTimeElem = document.querySelector('.giveaway-timer .end-time .time')
 const progressBar = document.querySelector('.giveaway-timer .timer-progress-bar .bar')
+
+let dt = new Date()
+let month = dt.getMonth() + 1
+let year = dt.getFullYear()
+let daysInMonth = new Date(year, month, 0).getDate()
+
 const TimeUnitSeconds = {
     year: 31536000,
-    month: 2592000,
-    week: 604800,
+    month: daysInMonth * 86400,
     day: 86400,
     hour: 3600,
     minute: 60,
@@ -50,7 +54,6 @@ const secondsToString = (delta, locale = DEFAULT_LANGUAGE, chunk = 5) => {
     const result = {
         year: 0,
         month: 0,
-        week: 0,
         day: 0,
         hour: 0,
         minute: 0,
@@ -68,7 +71,6 @@ const secondsToString = (delta, locale = DEFAULT_LANGUAGE, chunk = 5) => {
 
             if (result.year !== 0) arr.push(`${result.year} ${getUnit('year')}`)
             if (result.month !== 0) arr.push(`${result.month} ${getUnit('month')}`)
-            if (result.week !== 0) arr.push(`${result.week} ${getUnit('week')}`)
             if (result.day !== 0) arr.push(`${result.day} ${getUnit('day')}`)
             if (result.hour !== 0) arr.push(`${result.hour} ${getUnit('hour')}`)
             if (result.minute !== 0) arr.push(`${result.minute} ${getUnit('minute')}`)
@@ -119,26 +121,44 @@ const remainingInterval = setInterval(() => {
         return clearInterval(remainingInterval)
     }
 
+    const dt = new Date()
+    const month = dt.getMonth() + 1
+    const year = dt.getFullYear()
+    const daysInMonth = new Date(year, month, 0).getDate()
+
+    if (leftTime.second === 0) {
+        leftTime.second = 60
+        leftTime.minute = leftTime.minute - 1 < 0 ? 59 : leftTime.minute - 1
+    }
+
+    if (leftTime.second === 60 && leftTime.minute === 59) {
+        leftTime.hour = leftTime.hour - 1 < 0 ? 23 : leftTime.hour - 1
+    }
+
+    if (leftTime.second === 60 && leftTime.minute === 59 && leftTime.hour === 23) {
+        leftTime.day = leftTime.day - 1 < 0 ? daysInMonth - 1 : leftTime.day - 1
+    }
+
+    if (leftTime.second === 60 && leftTime.minute === 59 && leftTime.hour === 23 && leftTime.day === daysInMonth - 1) {
+        leftTime.month = leftTime.month - 1 < 0 ? 11 : leftTime.month - 1
+    }
+
     if (leftTime.month === 0) {
         remainingElem.month.parentElement.remove()
 
-        if (leftTime.week === 0) {
-            remainingElem.week.parentElement.remove()
+        if (leftTime.day === 0) {
+            remainingElem.day.parentElement.remove()
 
-            if (leftTime.day === 0) {
-                remainingElem.day.parentElement.remove()
+            if (leftTime.hour === 0) {
+                remainingElem.hour.parentElement.remove()
 
-                if (leftTime.hour === 0) {
-                    remainingElem.hour.parentElement.remove()
+                if (leftTime.minute === 0 || leftTime.minute === 59) {
+                    remainingElem.minute.parentElement.remove()
 
-                    if (leftTime.minute === 0) {
-                        remainingElem.minute.parentElement.remove()
-
-                        if (leftTime.second === 0) {
-                            giveawayEnd = true
-                            finishProcess()
-                            remainingElem.second.parentElement.remove()
-                        }
+                    if (leftTime.second === 0 || leftTime.second === 60) {
+                        giveawayEnd = true
+                        finishProcess()
+                        remainingElem.second.parentElement.remove()
                     }
                 }
             }
@@ -146,9 +166,8 @@ const remainingInterval = setInterval(() => {
     }
 
     remainingElem.month.innerText = leftTime.month
-    remainingElem.week.innerText = leftTime.week
     remainingElem.day.innerText = leftTime.day
     remainingElem.hour.innerText = leftTime.hour
     remainingElem.minute.innerText = leftTime.minute
-    remainingElem.second.innerText = leftTime.second === 0 ? 60 : leftTime.second
+    remainingElem.second.innerText = leftTime.second
 }, 500)
